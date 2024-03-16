c60c47efc1b5ff5d161f0c2f2a78b8cb2e5d943b
mukesh1108 <muksmalviya1991@gmail.com>
2024-03-17 01:19:49 +0530

    Add project files.

---
+++ RabbitMQ_Publisher_Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQ_Publisher_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");            
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "mukesh17032024",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            const string message = "Hello MG! I'm RabbitMQ publisher.";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "mukesh17032024",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
